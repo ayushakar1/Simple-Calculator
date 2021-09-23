caa7495f06c91272f8868a74174ae9d6d8b221f5
Ayusha Kar <B520015@iiit-bh.ac.in>
2021-09-23 21:18:58 +0530

    finished all edits

--- function.js
+++ function.js
@@ -16,6 +16,9 @@ function printoutput(num){
     }
 }
 function getFormattedNumber(num){
+    if(num=="-"){
+        return "";
+    }
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
@@ -25,3 +28,63 @@ function reverseNumberFormat(num){
     return Number(num.replace(/,/g,''));
 }
 // checked this using : alert(reverseNumberFormat(getoutput()));
+
+var operator = document.getElementsByClassName("operator");
+for(var i = 0; i<operator.length; i++)
+{
+    operator[i].addEventListener('click',function(){
+        //check message :  alert("The operator clicked:"+ this.id);
+
+        if(this.id == "clear"){
+            printequation("");
+            printoutput("");
+        }
+        else if(this.id == "backspace"){
+            var output = reverseNumberFormat(getoutput()).toString();
+            if(output){    //if output has a value
+                output = output.substr(0,output.length-1);
+                printoutput(output);
+            }
+        }
+        else{
+            var output = getoutput();
+            var equation = getequation();
+            if(output==""&&equation!=""){
+                if(isNaN(equation[equation.length-1])){
+                    equation=equation.substr(0,equation.length-1);
+                }
+            }
+            if (output!="" || equation!=""){
+                //using conditional ?true:false
+
+                output= output == ""?
+                output: reverseNumberFormat(output);
+                equation= equation+output;
+                if(this.id=="="){
+                    var result= eval(equation);
+                    printoutput(result);
+                    printequation("");
+                }
+                else{
+                    equation = equation + this.id;
+                    printequation(equation);
+                    printoutput("");
+                }
+            }
+        }
+    });
+}
+
+var number = document.getElementsByClassName("number");
+for(var i = 0; i<number.length; i++)
+{
+    number[i].addEventListener('click',function(){
+        //check message :  alert("The number clicked:"+ this.id);
+
+        var output = reverseNumberFormat(getoutput());
+        if(output!=NaN){    //if output is a number
+            output= output+ this.id;
+            printoutput(output);
+        }
+    });
+}
